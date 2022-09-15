03f3acf8d2ae5c0d69fe6db27fd4f8bb0e49850a
Amos Shin <amos.shin@vtsd.com>
2022-09-15 08:09:19 -0400

    done

--- Lab02vst.java
+++ Lab02vst.java
@@ -6,9 +6,30 @@
 
 */
 
-  public class Lab02vst {
+  public class Lab02vst
+  {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
+      System.out.println("Lab 02, 100 Point Version");
 
+      System.out.println();
+      System.out.println();
+      System.out.print("Meaure today's knowledge");
+      System.out.println("By yesterday's confusion");
+      System.out.println();
+      System.out.println("Bewilderment + Exposure = Obviousse");
+      System.out.println();
+      System.out.print("#  #####  #####     #####  #####  #  #     #  #####  #####");
+      System.out.println();
+      System.out.print("#  #   #  #   #     #   #  #   #  #  # #   #    #    #"    );
+      System.out.println();
+      System.out.print("#  #   #  #   #     #####  #   #  #  #  #  #    #    #####");
+      System.out.println();
+      System.out.print("#  #   #  #   #     #      #   #  #  #   # #    #        #");
+      System.out.println();
+      System.out.print("#  #####  #####     #      #####  #  #     #    #    #####");
+      System.out.println();
     }
-  }
+
+
+}
